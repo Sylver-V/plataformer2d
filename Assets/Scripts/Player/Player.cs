0132096373794ce79d3d06a906d2cf4584c12899
Sylver-V <vicjsanches@gmail.com>
2025-09-11 11:35:11 -0300

    add Run Key

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,10 +10,13 @@ public class Player : MonoBehaviour
     //public Vector2 velocity;
 
     public float speed;
+    public float speedRun;
 
     public Vector2 friction = new Vector2(.1f,0);
 
     public float forceJump = 2;
+
+    private float _currentSpeed;
 
     private void Update()
     {
@@ -23,16 +26,23 @@ public class Player : MonoBehaviour
 
     private void HandleMoviment()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+
+
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigibody.MovePosition(myRigibody.position - velocity * Time.deltaTime);
-            myRigibody.velocity = new Vector2(-speed, myRigibody.velocity.y);
+            myRigibody.velocity = new Vector2(-_currentSpeed, myRigibody.velocity.y);
         }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigibody.MovePosition(myRigibody.position + velocity * Time.deltaTime);
-            myRigibody.velocity = new Vector2(speed, myRigibody.velocity.y);
+            myRigibody.velocity = new Vector2(_currentSpeed, myRigibody.velocity.y);
         }
 
 
