d559f861f3df6381d2bcc765055e6bee50ff3c82
Sylver-V <vicjsanches@gmail.com>
2025-10-02 14:28:54 -0300

    HUD coin counter

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
+using System;
 
 public class ItemManager : Singleton<ItemManager>
 {
-
     public int coins;
+
+    // Evento para notificar quando moedas mudam
+    public Action<int> OnCoinsChanged;
 
     private void Start()
     {
@@ -16,11 +17,12 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
-
+        OnCoinsChanged?.Invoke(coins); // Atualiza HUD no início
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        OnCoinsChanged?.Invoke(coins); // Notifica HUD
     }
 }
