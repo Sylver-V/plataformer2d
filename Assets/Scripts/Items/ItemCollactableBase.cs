046ae7412f5eb57efceab2b972cebd3a92d9b406
Sylver-V <vicjsanches@gmail.com>
2025-10-03 18:08:48 -0300

    Add Particle Effects - Coin

--- Assets/Scripts/Items/ItemCollactableBase.cs
+++ Assets/Scripts/Items/ItemCollactableBase.cs
@@ -6,6 +6,15 @@ public class ItemCollactableBase : MonoBehaviour
 {
 
     public string comparteTag = "Player";
+    public ParticleSystem particleSystem;
+    //public float timeToHide = 3;
+    //public GameObject graphicItem;
+
+
+    private void Awake()
+    {
+        if (particleSystem != null) particleSystem.transform.SetParent(null);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,13 +29,20 @@ public class ItemCollactableBase : MonoBehaviour
     protected virtual void Collect()
     {
         //Debug.Log("Collect");
-        OnCollect();
+        //if(graphicItem != null) graphicItem.SetActive(false);
+        //Invoke(nameof(HideObject), timeToHide);
         gameObject.SetActive(false);
+        OnCollect();
     }
+
+    //private void HideObject()
+    //{
+    //    gameObject.SetActive(false);
+    //}
 
 
     protected virtual void OnCollect()
     {
-
+        if(particleSystem != null) particleSystem.Play();
     }
 }
